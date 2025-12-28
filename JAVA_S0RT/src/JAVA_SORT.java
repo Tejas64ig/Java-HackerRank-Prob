82fb8bd090b0a3a5aa3e80e9fc86402ce21f5637
Who cares <Thakurtejas567@gmail.com>
2025-12-28 21:57:39 +0530

    Update JAVA_SORT.java

--- JAVA_S0RT/src/JAVA_SORT.java
+++ JAVA_S0RT/src/JAVA_SORT.java
@@ -24,6 +24,22 @@ public class Solution {
 
         int maxSum = Integer.MIN_VALUE;
 
+      
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 3; j++) {
+
+                int currentSum =
+                        arr.get(i).get(j) + arr.get(i).get(j + 1)  + arr.get(i).get(j + 2) 
+                        + arr.get(i + 1).get(j + 1) + arr.get(i + 2).get(j)+ arr.get(i + 2).get(j + 1) 
+                                + arr.get(i + 2).get(j + 2);
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+            }
+        }
+
         System.out.println(maxSum);
     }
 }
+
