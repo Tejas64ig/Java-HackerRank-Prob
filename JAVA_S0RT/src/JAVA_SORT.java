41f814c4878d9e038783ded210a1d8631d4901c5
Who cares <Thakurtejas567@gmail.com>
2025-12-28 21:56:29 +0530

    Update JAVA_SORT.java

--- JAVA_S0RT/src/JAVA_SORT.java
+++ JAVA_S0RT/src/JAVA_SORT.java
@@ -1,2 +1,29 @@
-public class JAVA_SORT {
+  import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+
+        bufferedReader.close();
+
+        int maxSum = Integer.MIN_VALUE;
+
+        System.out.println(maxSum);
+    }
 }
